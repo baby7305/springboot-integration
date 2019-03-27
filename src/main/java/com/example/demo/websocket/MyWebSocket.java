9f2e7f9a8a64a6add6effcbedc1b59cf6d03a41d
baby7305 <songhaibei7305@163.com>
2019-03-27 19:08:40 +0800

    简化、调整代码，不过静态变量线程不安全

--- src/main/java/com/example/demo/websocket/MyWebSocket.java
+++ src/main/java/com/example/demo/websocket/MyWebSocket.java
@@ -12,7 +12,7 @@ import javax.websocket.server.ServerEndpoint;
 import java.io.IOException;
 import java.util.concurrent.CopyOnWriteArraySet;
 
-@ServerEndpoint(value = "/websocket")
+@ServerEndpoint(value = "/hello")
 @Component
 public class MyWebSocket {
 	//静态变量，用来记录当前在线连接数。应该把它设计成线程安全的。
@@ -24,7 +24,7 @@ public class MyWebSocket {
 	//与某个客户端的连接会话，需要通过它来给客户端发送数据
 	private Session session;
 
-	private static Logger log = LoggerFactory.getLogger("fileInfoLog");
+	private final Logger log = LoggerFactory.getLogger(MyWebSocket.class);
 
 
 	/**
@@ -34,13 +34,8 @@ public class MyWebSocket {
 	public void onOpen(Session session) {
 		this.session = session;
 		webSocketSet.add(this);     //加入set中
-		addOnlineCount();           //在线数加1
-		log.info("有新连接加入！当前在线人数为" + getOnlineCount());
-//        try {
-//            sendMessage("有新连接加入！当前在线人数为" + getOnlineCount());
-//        } catch (IOException e) {
-//            System.out.println("IO异常");
-//        }
+		this.onlineCount++;          //在线数加1
+		log.info("有新连接加入！当前在线人数为" + this.onlineCount);
 	}
 
 	/**
@@ -49,8 +44,8 @@ public class MyWebSocket {
 	@OnClose
 	public void onClose() {
 		webSocketSet.remove(this);  //从set中删除
-		subOnlineCount();           //在线数减1
-		log.info("有一连接关闭！当前在线人数为" + getOnlineCount());
+		this.onlineCount--;         //在线数减1
+		log.info("有一连接关闭！当前在线人数为" + this.onlineCount);
 	}
 
 	/**
@@ -59,30 +54,12 @@ public class MyWebSocket {
 	 * @param message 客户端发送过来的消息
 	 */
 	@OnMessage
-	public void onMessage(String message, Session session) throws IOException {
+	public void onMessage(String message, Session session) {
 		this.session = session;
 		message = "来自客户端的消息:" + message;
 		log.info(message);
-		try {
-			sendMessage(message);
-		} catch (IOException e) {
-			log.error("onMessage方法异常" + e.toString());
-			e.printStackTrace();
-		}
-		//群发消息
-//        sendInfo("群发消息"+message);
+		this.sendMessage(message);
 	}
-
-	/**
-	 * 发生错误时调用
-	 *
-	 * @OnError
-	 **/
-	public void onError(Session session, Throwable error) {
-		log.error("onMessage方法异常" + error.toString());
-		error.printStackTrace();
-	}
-
 
 	/**
 	 * 发送消息需注意方法加锁synchronized，避免阻塞报错
@@ -91,34 +68,7 @@ public class MyWebSocket {
 	 * @param message
 	 * @throws IOException
 	 */
-	public synchronized void sendMessage(String message) throws IOException {
-//         this.session.getBasicRemote().sendText(message);
+	public synchronized void sendMessage(String message) {
 		this.session.getAsyncRemote().sendText(message);
-	}
-
-
-	/**
-	 * 群发自定义消息
-	 */
-	public static void sendInfo(String message) throws IOException {
-		for (MyWebSocket item : webSocketSet) {
-			try {
-				item.sendMessage(message);
-			} catch (IOException e) {
-				continue;
-			}
-		}
-	}
-
-	public static synchronized int getOnlineCount() {
-		return onlineCount;
-	}
-
-	public static synchronized void addOnlineCount() {
-		MyWebSocket.onlineCount++;
-	}
-
-	public static synchronized void subOnlineCount() {
-		MyWebSocket.onlineCount--;
 	}
 }
